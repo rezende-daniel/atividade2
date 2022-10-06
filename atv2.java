6605b7da29dd0425aca42009af47ad9e69c73ed3
daniel-rezende <rezendedaniel2002@gmail.com>
2022-10-06 20:10:30 -0300

    feat: atv2.java

---
+++ atv2.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class atv2 {
+	
+        public static void main(String args[]){
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira um número ");
+        int numero = entrada.nextInt();
+        entrada.close();
+
+        
+     
+
+     System.out.println("O numero escrito foi " + numero + "");
+
+ }
+}
+
